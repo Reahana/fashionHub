4e8a5cfafd84f4f551542605397b72421cdc67b0
Amanda Reahana Gomes <reahana233@gmail.com>
2022-01-23 10:34:19 +0600

    front includes

---
+++ resources/views/front/includes/meta.blade.php
@@ -0,0 +1,4 @@
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8" />
+<meta name="keywords" content="Fashion Hub Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
+	SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
---
+++ resources/views/front/includes/style.blade.php
@@ -0,0 +1,12 @@
+<link href="{{asset('/')}}css/bootstrap.css" type="text/css" rel="stylesheet" media="all">
+<!-- shop css -->
+<link href="{{asset('/')}}css/shop.css" type="text/css" rel="stylesheet" media="all">
+<link rel="stylesheet" href="{{asset('/')}}css/owl.carousel.min.css">
+<!-- Owl-Carousel-CSS -->
+<link href="{{asset('/')}}css/style.css" type="text/css" rel="stylesheet" media="all">
+<!-- font-awesome icons -->
+<link href="{{asset('/')}}css/fontawesome-all.min.css" rel="stylesheet">
+<!-- //Custom Theme files -->
+<!-- online-fonts -->
+<link href="//fonts.googleapis.com/css?family=Elsie+Swash+Caps:400,900" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:200,200i,300,300i,400,400i,600,600i,700,700i,900,900i" rel="stylesheet">
